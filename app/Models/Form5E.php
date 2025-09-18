f2b30c96f1eef8e11af287e3c3406682737d35bf
max8888git <149941758+max8888git@users.noreply.github.com>
2025-09-19 01:44:17 +0800

    Update Form5E.php

--- app/Models/Form5E.php
+++ app/Models/Form5E.php
@@ -15,6 +15,59 @@ class Form5E extends Model
 
     protected $fillable = [
         'form5EID',
-        'protocol',
+        'protocol_title',
+        'pi_name',
+        'coiname',
+        'pi_contact',
+        'pi_email',
+        'institution',
+        'institute_address',
+        'erb_contact',
+
+        //Initial Documents (Checkboxes)
+        'cover_letter',
+        'enrollment_proof',
+        'letter',
+        'complete_form2b',
+        'complete_form2a',
+        'complete_form2d',
+
+        //Protocol Package (Checkboxes)
+        'study_protocol',
+        'form2c_eng',
+        'form2c_fil',   
+        'data_collection',
+        'cert_validator',
+        'eng_7_12_yrs',
+        'fil_7_12_yrs',
+        'eng_13_17_yrs',
+        'fil_13_17_yrs',
+        'advertisement',
+        'vitae',
+        'gcp',
+    ];
+
+    protected $casts = [
+        //Initial Documents
+        'cover_letter' => 'boolean',
+        'enrollment_proof' => 'boolean',
+        'letter' => 'boolean',
+        'complete_form2b' => 'boolean',
+        'complete_form2a' => 'boolean',
+        'complete_form2d' => 'boolean',
+
+        //Protocol Package
+        'study_protocol' => 'boolean',
+        'form2c_eng' => 'boolean',
+        'form2c_fil' => 'boolean',   
+        'data_collection' => 'boolean',
+        'cert_validator' => 'boolean',
+        'eng_7_12_yrs' => 'boolean',
+        'fil_7_12_yrs' => 'boolean',
+        'eng_13_17_yrs' => 'boolean',
+        'fil_13_17_yrs' => 'boolean',
+        'advertisement' => 'boolean',
+        'vitae' => 'boolean',
+        'gcp' => 'boolean',
     ];
 }
