1b62baeaec909f4a0ce1449ef75d05e1e43e0a15
max8888git <149941758+max8888git@users.noreply.github.com>
2025-09-19 01:46:10 +0800

    Update 2025_08_27_163001_form5e.php

--- database/migrations/2025_08_27_163001_form5e.php
+++ database/migrations/2025_08_27_163001_form5e.php
@@ -13,7 +13,36 @@ return new class extends Migration
     {
         Schema::create('tbl_form5e', function (Blueprint $table) {
             $table->string('form5EID')->primary();
-            $table->string('protocol');
+            $table->string('protocol_title');
+            $table->string('pi_name');
+            $table->string('coiname')->nullable();  
+            $table->string('pi_contact');
+            $table->string('pi_email');
+            $table->string('institution');
+            $table->string('institute_address');
+            $table->string('erb_contact');
+
+            //Initial Documents 
+            $table->boolean('cover_letter')->default(false);
+            $table->boolean('enrollment_proof')->default(false);
+            $table->boolean('letter')->default(false);
+            $table->boolean('complete_form2b')->default(false);
+            $table->boolean('complete_form2a')->default(false);
+            $table->boolean('complete_form2d')->default(false);
+
+            //Protocol Package
+            $table->boolean('study_protocol')->default(false);
+            $table->boolean('form2c_eng')->default(false);
+            $table->boolean('form2c_fil')->default(false);   
+            $table->boolean('data_collection')->default(false);
+            $table->boolean('cert_validator')->default(false);
+            $table->boolean('eng_7_12_yrs')->default(false);
+            $table->boolean('fil_7_12_yrs')->default(false);
+            $table->boolean('eng_13_17_yrs')->default(false);
+            $table->boolean('fil_13_17_yrs')->default(false);
+            $table->boolean('advertisement')->default(false);
+            $table->boolean('vitae')->default(false);
+            $table->boolean('gcp')->default(false);
         });
     }
 
