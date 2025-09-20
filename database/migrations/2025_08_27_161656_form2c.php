018e9d3a539e38d997bf26e52eea27e9d8074e9a
max8888git <149941758+max8888git@users.noreply.github.com>
2025-09-21 00:23:07 +0800

    Update 2025_08_27_161656_form2c.php

--- database/migrations/2025_08_27_161656_form2c.php
+++ database/migrations/2025_08_27_161656_form2c.php
@@ -13,7 +13,47 @@ return new class extends Migration
     {
         Schema::create('tbl_form2c', function (Blueprint $table) {
             $table->string('form2CID')->primary();
-            $table->string('protocol');
+            $table->string('protocol_title');
+            $table->string('pi_name');
+            $table->string('coiname');
+            $table->string('pi_contact');
+            $table->string('pi_email');
+            $table->string('institution');
+            $table->string('institute_address');
+            $table->string('erb_contact');
+
+            //Part II
+            
+            //Part III Research Study
+            $table->text('research_purpose')->nullable();
+            $table->text('research_procedures')->nullable();
+            $table->text('participant_selection')->nullable();
+            $table->text('participation_study')->nullable();
+            $table->text('duration')->nullable();
+            $table->text('potential_risks')->nullable();
+            $table->text('potential_benefits')->nullable();
+            $table->text('management_study_injuries')->nullable();
+            $table->text('compensation')->nullable();
+            $table->text('confidentiality')->nullable();
+            $table->text('voluntary_participation')->nullable();
+            $table->text('title_name');
+            $table->text('institution');
+            $table->text('pi_contact');
+            $table->text('pi_email');
+            $table->text('approval_mcuerb')->nullable();
+            $table->text('contact_mcuerb')->nullable();
+
+            //Part IV Certificate of Consent 
+            $table->boolean('information_sheet')->default(false);
+            $table->boolean('study_discuss')->default(false);
+            $table->boolean('satisfactory_answers')->default(false);
+            $table->boolean('study_information')->default(false);
+            $table->boolean('study_nature')->default(false);
+            $table->boolean('withdraw')->default(false);
+            $table->boolean('medical')->default(false);
+            $table->boolean('interview')->default(false);
+            $table->boolean('decision')->default(false);
+            $table->boolean('voluntary')->default(false);
         });
     }
 
